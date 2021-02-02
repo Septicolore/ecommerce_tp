588a473e7d6878e01b02f32f41f857283ac83d0f
Septicolore <jule.boulanger@gmail.com>
2021-02-02 11:19:11 +0100

    dashboard porduit et utilisateur okay

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\Product;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -27,6 +29,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
+        yield MenuItem::linkToCrud('Produits', 'fas fa-dolly', Product::class);
+        yield MenuItem::linkToCrud('Utilisateurs', 'fas fa-user-edit', User::class);
         // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
     }
 }
